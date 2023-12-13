43792f1c411b9ef325574d07d4aeb3bfbb8c7c77
mx-pl <mx-pl@posteo.de>
2023-12-13 13:18:01 +0100

    Add straightforward boolean toggling.
    Extension methods 'Toggle' and 'Toggled' added and implemented.
    
    Important: Not yet tested.

---
+++ ExtendedTypes/Boolean/BoolToggling.cs
@@ -0,0 +1,33 @@
+﻿/*
+ ---------------------------------------------------------------------------
+  Copyright (c) 2023 mx-pl
+
+  Licensed under the MIT License.
+
+  You can view its full text at:
+  https://github.com/mx-pl/ExtendedTypes_CSharp/blob/main/LICENSE.
+ ---------------------------------------------------------------------------
+*/
+
+namespace ExtendedTypes.Boolean
+{
+    public static class BoolToggling
+    {
+        /// <summary>
+        /// Sets this instance to its negated value.
+        /// </summary>
+        public static void Toggle(this ref bool b)
+        {
+            b = !b;
+        }
+
+        /// <summary>
+        /// Returns the negated value of this instance without changing the instance itself.
+        /// </summary>
+        /// <returns>The negated value of this instance.</returns>
+        public static bool Toggled(this bool b)
+        {
+            return !b;
+        }
+    }
+}
