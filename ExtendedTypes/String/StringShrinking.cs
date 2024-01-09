342f1f342fe0d06125d15c097907c223f6c87142
mx-pl <mx-pl@posteo.de>
2024-01-09 21:36:01 +0100

    Add refined draft for string whitespace removal methods.
    Initial draft of extension method 'Shrink' split into 'Shrink'
    and 'CondenceWhiteSpace'.
    
    Important: Proofreading and testing necessary.

--- ExtendedTypes/String/StringShrinking.cs
+++ ExtendedTypes/String/StringShrinking.cs
@@ -9,47 +9,101 @@
  ---------------------------------------------------------------------------
 */
 
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace ExtendedTypes.String
 {
     /// <summary>
-    /// A class encapsulating extension methods for easy removal of white-space characters from strings.
+    /// A class encapsulating extension methods for easy removal of whitespace characters from strings.
     /// </summary>
     public static class StringShrinking
     {
-       /// <summary>
-       /// 
-       /// </summary>
-       /// <param name="str"></param>
-       /// <param name="separator"></param>
-       /// <param name="doTrim"></param>
-       /// <returns></returns>
-       /// <exception cref="ArgumentNullException"></exception>
-        public static string Shrink(
-            this string str,
-            char? separator = ' ',
-            bool doTrim = true)
+        #region Public Extension Methods
+
+        public static string CondenceWhiteSpace(this string str, string? replacement = " ")
         {
+            /* --- Input Validation --- */
             if (str is null) throw new ArgumentNullException(nameof(str));
 
-            var trimmedString = doTrim ? str.Trim() : str;
+            if (str.Length == 0) return str;
 
-            if (separator is not null)
+            /* --- Replacing all sequences of whitespace --- */
+            if (replacement is not null)
             {
-                // Replace all white-space characters with the specified separator.
-                return Regex.Replace(trimmedString, @"\s+", separator.Value.ToString());
+                return Regex.Replace(
+                    str,
+                    @"\s+",
+                    replacement.Length > 1
+                    ? replacement[0].ToString()
+                    : replacement);
             }
             else
             {
-                // No separator has been specified:
-                // Keep the different kinds of white-space characters,
-                // but condence consecutive ones (of the same kind) to only one.
+                // We do not replace the whitespace characters but condence sequences of the same kind
+                // of whitespace character to just one of its kind.
 
-                // TODO: Implement for different kinds of white-space characters!
+                // I.e. when building our result string we will only include non-whitespace characters
+                // and whitespace characters which are different from their preceding one.
 
-                return trimmedString; // Preliminary !!
+                // Note that in any case:
+                //
+                //  condencedString.Length <= str.Length
+                //      (because we are going to remove characters)
+
+                var condencedString = new StringBuilder(str[0], str.Length);
+
+                char previous = str[0];
+
+                for (int i = 1; i < str.Length; i++)
+                {
+                    if (!char.IsWhiteSpace(str[i]) || str[i] != previous)
+                    {
+                        condencedString.Append(str[i]);
+                        previous = str[i];
+                    }
+                }
+
+                return condencedString.ToString();
             }
         }
+
+
+        public static string Shrink(this string str) => Shrink(str, ' ', true);
+
+
+        public static string Shrink(this string str, char separator) => Shrink(str, separator, true);
+
+
+        public static string Shrink(this string str, char separator, bool trim)
+        {
+            /* --- Input Validation --- */
+
+            if (str is null) throw new ArgumentNullException(nameof(str));
+            if (str.Length == 0) return str;
+
+            /* --- Trimming --- */
+
+            var inputSpan = str.AsSpan();
+
+            if (trim)
+            {
+                var trimmedSpan = inputSpan.Trim();
+
+                // Since Trim() has removed all *leading* and *trailing* whitespace characters,
+                // at least three chars have to remain in the string for further whitespace
+                // characters to be present (i.e. non-whitespace | whitespace | non-whitespace ).
+                if (trimmedSpan.Length <= 2)
+                    return trimmedSpan.ToString();
+
+                inputSpan = trimmedSpan;
+            }
+
+            /* --- Condense Whitespace --- */
+
+            return inputSpan.ToString().CondenceWhiteSpace(separator.ToString());
+        }
+
+        #endregion
     }
 }
