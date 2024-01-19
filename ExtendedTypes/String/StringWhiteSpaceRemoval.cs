aea148a56e21f9b15f8e73359685837e3574409a
mx-pl <mx-pl@posteo.de>
2024-01-19 15:09:24 +0100

    Add updated Shrink() method.
    Important: Not yet tested.

--- ExtendedTypes/String/StringWhiteSpaceRemoval.cs
+++ ExtendedTypes/String/StringWhiteSpaceRemoval.cs
@@ -52,39 +52,64 @@ namespace ExtendedTypes.String
 
         #region Shrink()
 
+        /// <summary>
+        /// Removes all whitespace from the start and end of the current <see cref="string"/>. Additionally,
+        /// all remaining occurrances of whitespace are replaced by the SPACE <see cref="char"/>.
+        /// </summary>
+        /// <param name="str">The string instance this method is invoked on.</param>
+        /// <returns>
+        /// A <see cref="string"/> with no leading or trailing whitespace and where all sequences of non-whitespace characters
+        /// are kept apart by single space chars.
+        /// </returns>
         public static string Shrink(this string str) => Shrink(str, ' ', true);
 
-
+        /// <summary>
+        /// Removes all whitespace from the start and end of the current <see cref="string"/>. Additionally,
+        /// all remaining occurrances of whitespace are replaced by the <paramref name="separator"/> <see cref="char"/>.
+        /// </summary>
+        /// <param name="str">The string instance this method is invoked on.</param>
+        /// <param name="separator">The <see cref="char"/> used to replace the whitespace.</param>
+        /// <returns>
+        /// A <see cref="string"/> with no leading or trailing whitespace and where all sequences of non-whitespace characters
+        /// are kept apart by the specified <paramref name="separator"/>.
+        /// </returns>
         public static string Shrink(this string str, char separator) => Shrink(str, separator, true);
 
-
+        /// <summary>
+        /// Replaces all sequences of whitespace in the current <see cref="string"/> with the <paramref name="separator"/> <see cref="char"/>.
+        /// Optionally, the string is trimmed beforehand.
+        /// </summary>
+        /// <param name="str">The string instance this method is invoked on.</param>
+        /// <param name="separator">The <see cref="char"/> used to replace the whitespace.</param>
+        /// <param name="trim">
+        /// Indicating whether leading and trailing whitespace should be removed from the string.
+        /// <see langword="true"/> if so; <see langword="false"/> if not.
+        /// </param>
+        /// <returns>
+        /// A <see cref="string"/> where all sequences of non-whitespace characters
+        /// are kept apart by the specified <paramref name="separator"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static string Shrink(this string str, char separator, bool trim)
         {
-            /* --- Input Validation --- */
-
             if (str is null) throw new ArgumentNullException(nameof(str));
-            if (str.Length == 0) return str;
-
-            /* --- Trimming --- */
-
-            var inputSpan = str.AsSpan();
+            if (str.Length == 0) return str; // Nothing to do.
 
             if (trim)
             {
-                var trimmedSpan = inputSpan.Trim();
+                var trimmedStr = str.Trim();
 
                 // Since Trim() has removed all *leading* and *trailing* whitespace characters,
                 // at least three chars have to remain in the string for further whitespace
                 // characters to be present (i.e. non-whitespace | whitespace | non-whitespace ).
-                if (trimmedSpan.Length <= 2)
-                    return trimmedSpan.ToString();
+                if (trimmedStr.Length <= 2) return trimmedStr;
 
-                inputSpan = trimmedSpan;
+                return trimmedStr.SquashWhiteSpace(separator);
             }
-
-            /* --- Squash Whitespace --- */
-
-            return inputSpan.ToString().SquashWhiteSpace(separator);
+            else
+            {
+                return str.SquashWhiteSpace(separator);
+            }
         }
 
         #endregion
